8d5208d53d7ad3ad67d04ea6207995e2a1b8c0e9
Guy Mac <hello@guymac.eu>
2018-05-16 17:54:23 +0100

    Add opcode enum

---
+++ src/com/guym4c/uni/networks/coursework2/TftpOpcode.java
@@ -0,0 +1,34 @@
+package com.guym4c.uni.networks.coursework2;
+
+public enum TftpOpcode {
+
+    ReadRequest(1),
+    WriteRequest(2),
+    Data(3),
+    Acknowledgement(4),
+    Error(5);
+
+    private int value;
+
+    TftpOpcode(final int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int toInt() {
+        return this.getValue();
+    }
+
+    public static TftpOpcode fromInt(int i) {
+        for (TftpOpcode opcode : TftpOpcode.values()) {
+            if (opcode.value == i) {
+                return opcode;
+            }
+        }
+        throw new IllegalArgumentException("No opcode with that index");
+    }
+
+}

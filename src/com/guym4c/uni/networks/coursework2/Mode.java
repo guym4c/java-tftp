05a33b90adb670c3f55c7ffb6293701de5b734fd
Guy Mac <hello@guymac.eu>
2018-05-17 06:45:57 +0100

    Make Mode queryable enum

--- src/com/guym4c/uni/networks/coursework2/Mode.java
+++ src/com/guym4c/uni/networks/coursework2/Mode.java
@@ -1,7 +1,34 @@
 package com.guym4c.uni.networks.coursework2;
 
 public enum Mode {
-    NETASCII,
-    OCTET,
-    MAIL;
+    NetAscii("netascii"),
+    Octet("octet"),
+    Mail("mail");
+
+    private String value;
+
+    Mode(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return this.getValue();
+    }
+
+    public static Mode fromString(String s) {
+        for (Mode mode : Mode.values()) {
+            if (mode.value.equals(s.toLowerCase())) {
+                return mode;
+            }
+        }
+        throw new IllegalArgumentException("Unknown mode");
+    }
+
+}
+
 }

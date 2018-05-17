e7b623a3bb57fc683e4a6b17d7cd823e8208ac06
Guy Mac <hello@guymac.eu>
2018-05-17 01:21:40 +0100

    Correct DATA data extraction

--- src/com/guym4c/uni/networks/coursework2/TftpDataPacketBuffer.java
+++ src/com/guym4c/uni/networks/coursework2/TftpDataPacketBuffer.java
@@ -5,12 +5,15 @@ import java.util.Arrays;
 public class TftpDataPacketBuffer extends TftpTransmissionPacketBuffer {
 
     private static final int STRING_DATA_OFFSET = 4;
+    private static final int MAX_PAYLOAD_SIZE = 512;
 
     private String data;
+    private boolean terminating;
 
-    public TftpDataPacketBuffer(TftpOpcode opcode, int block, String data) {
+    public TftpDataPacketBuffer(TftpOpcode opcode, int block, String data, boolean terminating) {
         super(opcode, block);
         this.data = data;
+        this.terminating = terminating;
     }
 
     public String getData() {
@@ -21,9 +24,18 @@ public class TftpDataPacketBuffer extends TftpTransmissionPacketBuffer {
         this.data = data;
     }
 
+    public boolean isTerminating() {
+        return terminating;
+    }
+
+    public void setTerminating(boolean terminating) {
+        this.terminating = terminating;
+    }
+
     public TftpDataPacketBuffer(byte[] bytes) {
         super(bytes);
-        this.data = getZeroDelimitedData(Arrays.copyOfRange(bytes, STRING_DATA_OFFSET, bytes.length - STRING_DATA_OFFSET))
-                .get(0);
+        this.data = getStringFromBytes(Arrays.copyOfRange(bytes, STRING_DATA_OFFSET, bytes.length - 1));
+        this.terminating = bytes.length - STRING_DATA_OFFSET - 1 == MAX_PAYLOAD_SIZE;
+
     }
 }

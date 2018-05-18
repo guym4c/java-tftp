243f07854abd496c936c64ecd0c5da89d9ce1f25
Guy Mac <hello@guymac.eu>
2018-05-18 18:53:24 +0100

    Initial client

---
+++ src/com/guym4c/uni/networks/coursework2/Client.java
@@ -0,0 +1,52 @@
+package com.guym4c.uni.networks.coursework2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.SocketException;
+
+public class Client extends NetworkObject {
+
+    public Client() throws SocketException {
+        super(generateTid());
+    }
+
+    @Override
+    public void run() {
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("Choose: ");
+            System.out.println("1: Get a file");
+            System.out.println("2: Send a file");
+            System.out.println("3: Exit");
+            System.out.println();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                int choice = Integer.parseInt(reader.readLine());
+                System.out.println("Please enter the filename:");
+                String file = reader.readLine();
+                int tid = generateTid();
+                switch (Opcode.fromInt(choice)) {
+                    case ReadRequest:
+                        new ReadRequestClient(SERVER_IP, SERVER_PORT, file, tid).start();
+                        break;
+                    case WriteRequest:
+                        new WriteRequestClient(SERVER_IP, SERVER_PORT, file, tid).start();
+                        break;
+                    default:
+                        exit = true;
+                        break;
+                }
+            } catch (IOException e) {
+                exit = true;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
+}
